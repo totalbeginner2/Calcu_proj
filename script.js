0670ff1ecbd7a8dc5fea0d339f36d1a5d0f98c9a
depedpc <rs200m2@yahoo.com>
2024-01-12 14:34:01 +0800

    cleaned code, replaced classlists of button with datakeys that correspond to their value for more readable code, created global variables, removed unnecessary functions, and reworked clearDisplay

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-//
+let num1 = 0;
+let num2 = 0;
+let operator = null;
+const displayBox = document.querySelector('.displayNum');
 
 function add(num1,num2){
     return num1 + num2;
@@ -16,72 +19,86 @@ function divide(num1,num2){
     return num1 / num2;
 };
 
-function operate(num1,operator,num2){
-if (operator == 'add'){
-    return add(num1,num2);
-};
-if (operator == 'subtract'){
-    return subtract(num1,num2);
-};
-if (operator == 'multiply'){
-    return multiply(num1,num2);
-};
-if (operator == 'divide'){
-    return divide(num1,num2);
-};
+function add(num1,num2){
+    return num1 + num2;
 };
 
-function calc(){
-const numBox = document.querySelector('.nums');
+function subtract(num1,num2){
+    return num1 - num2;
+};
+
+function multiply(num1,num2){
+    return num1 * num2;
+};
+
+function divide(num1,num2){
+    return num1 / num2;
+};
+
+
+
+let numBox = document.querySelector('.nums');
 for(let x = 1; x < 13 ;x++){
 let numbers = document.createElement('div');
 numbers.textContent = `${x}`;
 numbers.className="numBox";
-numbers.classList.add(`${x}`);
+numbers.dataset.num = x;
 numBox.appendChild(numbers);
 numbers.style.height = '100px';
 numbers.style.width = '100px';
 if(x == '10'){
     numbers.textContent = '0';
-    numbers.classList.remove("10");
-    numbers.classList.add("0");
+    numbers.dataset.num = 0;
 };
 if(x == '11'){
-    numbers.textContent = '.'
-    numbers.classList.remove('11');
-    numbers.classList.add(".");
+    numbers.textContent = '.';
+    numbers.dataset.num = '.';
 };
 if(x == '12'){
     numbers.textContent = 'C'
     numbers.className = "clearBtn";
 };
 };
-};
 
-calc();
 
-function createDisplay(num){
-    let numBox = document.querySelector('.displayNum');
-    numBox.textContent += num;
-    
-}
-
-function numClick (){
 let numButtons = document.querySelectorAll('.numBox');
 numButtons.forEach((numButton) => {
     numButton.addEventListener("click", function(){
-        createDisplay(numButton.classList[1]);
+        displayBox.textContent += numButton.dataset.num;
     });
 }
 );
-};
+
 
 let clearButton = document.querySelector('.clearBtn');
 clearButton.addEventListener("click", function (){
-    let numBox = document.querySelector('.displayNum');
-    numBox.textContent = "";
+        clearDisplay()
+    });
+
+
+function clearDisplay(){
+    displayBox.textContent = "";
+};
+
+
+function operate(num1,operator,num2){
+  switch(operator){
+
+  };
+};
+
+function setOperator(opera){
+
+};
+
+
+let operaBtn = document.querySelectorAll('.oper');
+operaBtn.forEach((operaButton) => {
+        operaButton.addEventListener("click", function (){
+                console.log(operaButton.dataset.opera);
+             });
 })
-numClick();
+
 
 
 //let samplebutton = document.querySelector('.opera');
